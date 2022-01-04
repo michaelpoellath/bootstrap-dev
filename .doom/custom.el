8b29f4d916a7349b5c735c4d1006d4e86cf1ce26
Michael Pöllath <michael.poellath@syncier.com>
2022-01-04 16:46:35 +0100

    update

---
+++ .doom/custom.el
@@ -0,0 +1,55 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212337" "#ff757f" "#c3e88d" "#ffc777" "#82aaff" "#c099ff" "#b4f9f8" "#c8d3f5"])
+ '(custom-safe-themes
+   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "1ca05bdae217adeb636e9bc5e84c8f1d045be2c8004fafd5337d141d9b67a96f" "bd3b9675010d472170c5d540dded5c3d37d83b7c5414462737b60f44351fb3ed" "aca70b555c57572be1b4e4cec57bc0445dcb24920b12fb1fea5f6baa7f2cad02" "f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" default))
+ '(doom-modeline-mode t)
+ '(exwm-floating-border-color "#161a2a")
+ '(fci-rule-color "#444a73")
+ '(highlight-tail-colors
+   ((("#31363f" "#31363f" "green")
+     . 0)
+    (("#2f384a" "#2f384a" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#161a2a" "#82aaff"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#161a2a" "#c3e88d"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#161a2a" "#444a73"))
+ '(nano-modeline-mode t)
+ '(objed-cursor-color "#ff757f")
+ '(package-selected-packages '(nano-agenda nano-modeline nano-theme evil-multiedit))
+ '(pdf-view-midnight-colors (cons "#c8d3f5" "#212337"))
+ '(rustic-ansi-faces
+   ["#212337" "#ff757f" "#c3e88d" "#ffc777" "#82aaff" "#c099ff" "#b4f9f8" "#c8d3f5"])
+ '(vc-annotate-background "#212337")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#c3e88d")
+    (cons 40 "#d7dd85")
+    (cons 60 "#ebd27e")
+    (cons 80 "#ffc777")
+    (cons 100 "#ffb76e")
+    (cons 120 "#ffa866")
+    (cons 140 "#ff995e")
+    (cons 160 "#ea9993")
+    (cons 180 "#d599c9")
+    (cons 200 "#c099ff")
+    (cons 220 "#d58dd4")
+    (cons 240 "#ea81a9")
+    (cons 260 "#ff757f")
+    (cons 280 "#d06a7c")
+    (cons 300 "#a15f79")
+    (cons 320 "#725476")
+    (cons 340 "#444a73")
+    (cons 360 "#444a73")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic)))))
